a500abc03a085b8d02673f7cbf70b1e1a928922c
Михаил Галактифонов <galaxy_mike@rambler.ru>
2014-04-24 21:33:26 +0900

    enum type

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,6 @@
 from sqlalchemy.ext.declarative import as_declarative, declared_attr
+from sqlalchemy.types import TypeDecorator, Integer
+import enum
 import re
 
 @as_declarative()
@@ -14,3 +16,28 @@ class Base(object):
             return re.sub('y$', 'ies', name)
         else:
             return name + 's'
+
+class _define_enum_type(TypeDecorator):
+    impl = Integer
+
+    def __init__(self, enum):
+        self.enum = enum
+
+    def process_bind_param(self, member, dialect):
+        assert member in self.enum
+        return member.value
+
+    def process_result_value(self, value, dialect):
+        return self.enum(value);
+
+class Enum(enum.Enum):
+    def __new__(cls):
+        value = len(cls.__members__) + 1
+        obj = object.__new__(cls)
+        obj._value_ = value
+
+        return obj
+
+    @classmethod
+    def enum_type(self):
+        return _define_enum_type(self)
--- app/models/users.py
+++ app/models/users.py
@@ -1,5 +1,5 @@
-from models import Base
-from sqlalchemy import Column, DateTime, Enum, Integer, String, func
+from models import Base, Enum
+from sqlalchemy import Column, DateTime, Integer, String, TypeDecorator, func
 
 class Password(TypeDecorator):
     impl = String
@@ -9,15 +9,28 @@ class Password(TypeDecorator):
 
     class comparator_factory(String.comparator_factory):
         def __eq__(self, other):
-            hash = type_coerce(self.expr, String)
-            return hash == func.crypt(other, hash)
+            crypted_password = type_coerce(self.expr, String)
+            return crypted_password == func.crypt(other, crypted_password)
+
+class Role(Enum):
+    admin           = ()
+    manager         = ()
+    employee        = ()
+    client_manager  = ()
+    client_employee = ()
+
+class Status(Enum):
+    pending     = ()
+    active      = ()
+    suspended   = ()
+    deleted     = ()
 
 class User(Base):
     id         = Column(Integer, primary_key=True)
     email      = Column(String)
     fullname   = Column(String)
     password   = Column(Password, nullable=False)
-    role       = Column(Enum('manager', 'employee', 'client manager', 'client employee'), default='client manager')
-    status     = Column(Enum('pending', 'active', 'suspended', 'deleted'), default='pending')
-    created_at = Column(DateTime, default=func.now())
-    updated_at = Column(DateTime, default=func.now(), onupdate=func.current_timestamp())
+    role       = Column(Role.enum_type(), default=Role.client_manager)
+    status     = Column(Status.enum_type(), default=Status.pending)
+    created_at = Column(DateTime, server_default=func.now())
+    updated_at = Column(DateTime, server_default=func.now(), onupdate=func.current_timestamp())
